23d60bf142abe050cdbfeeb172676a71224ac87a
hwen <hwendev@gmail.com>
2022-12-10 22:09:32 +0800

    connect socket from frontend

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,25 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 dotenv.config()
 
 const PORT = process.env.PORT;
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.CORS_ORIGIN,
+        methods: ["GET", "POST"]
+    }
+});
 
 /**
  *  App Configuration
  */
-
 app.use(helmet()); //safety
 app.use(cors()); //safety
 app.use(
@@ -38,6 +46,17 @@ app.use('*', function (req: Request, res: Response) {
     res.sendStatus(404);
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+io.on("connection", (socket) => {
+    socket.on('join_room', ({ uid, roomID }: { uid: string, roomID: string }) => {
+        console.log(`${uid} has joined the ${roomID}`);
+        socket.emit("message", "Welcome to Stream2Gether. " + socket.id);
+        socket.join(roomID);
+    });
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} has left the room`);
+    });
 });
+
+httpServer.listen(PORT);

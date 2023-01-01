f93ca673e889dbaabb46daaa240f4468d5590bf4
hwen <hwendev@gmail.com>
2023-01-01 20:16:48 +0800

    refactor navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,25 +1,32 @@
-import { Box, Button, Flex, HStack, Spacer, Text } from '@chakra-ui/react';
-import { useQueryClient } from '@tanstack/react-query';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Stack,
+  Text,
+  useColorMode,
+  useColorModeValue
+} from '@chakra-ui/react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import router from 'next/router';
 import { FC } from 'react';
 import { firebaseLogout } from '../auth/firebaseAuth';
 import { MeQueryKey } from '../constants/query';
 import { useAuth } from '../contexts/AuthContext';
-
 import { useLogoutMutation } from '../generated/graphql';
-import { PrimaryButton } from './ui/buttons/PrimaryButton';
-import { ThemeToggler } from './ui/theme/ThemeToggler';
+import { queryClient } from '../pages/_app';
 
-const Navbar: FC = () => {
-  const router = useRouter();
-  const queryClient = useQueryClient();
-
+const UserMenu: FC = () => {
   const { user } = useAuth();
-
   const { mutateAsync } = useLogoutMutation({});
-
-  const isLoggedIn = user !== undefined && user !== null;
 
   const logout = async (): Promise<void> => {
     const result = await mutateAsync({});
@@ -31,47 +38,114 @@ const Navbar: FC = () => {
   };
 
   return (
-    <Flex direction="row" px="8" py="4">
-      <Box>
-        {/* ADD APP ICON HERE */}
-        <Link href="/">
-          <Text fontSize="xl" fontWeight="bold">
-            Stream2Gather
-          </Text>
-        </Link>
+    <Menu>
+      <MenuButton
+        as={Button}
+        rounded={'full'}
+        variant={'link'}
+        cursor={'pointer'}
+        minW={0}
+      >
+        <Avatar
+          size={'sm'}
+          src={`https://avatars.dicebear.com/api/pixel-art/${
+            user?.username ?? ''
+          }.svg`}
+        />
+      </MenuButton>
+
+      <MenuList alignItems={'center'}>
+        <br />
+        <Center>
+          <Avatar
+            size={'2xl'}
+            src={`https://avatars.dicebear.com/api/pixel-art/${
+              user?.username ?? ''
+            }.svg`}
+          />
+        </Center>
+        <br />
+        <Center>
+          <Text fontSize={'md'}>{user?.username ?? ''}</Text>
+        </Center>
+        <br />
+        <MenuDivider />
+        <MenuItem>Edit Profile</MenuItem>
+        <MenuItem>Account Settings</MenuItem>
+        <MenuItem onClick={async () => await logout()}>Logout</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+const LoginItems: FC = () => {
+  return (
+    <Stack
+      flex={{ base: 1, md: 0 }}
+      justify={'flex-end'}
+      direction={'row'}
+      spacing={6}
+    >
+      <Button fontSize={'sm'} fontWeight={400} variant={'link'}>
+        <Link href="/login">Sign In</Link>
+      </Button>
+      <Button
+        display={{ base: 'none', md: 'inline-flex' }}
+        fontSize={'sm'}
+        fontWeight={600}
+        color={'white'}
+        bg={'secondary'}
+        _hover={{
+          opacity: 0.8
+        }}
+      >
+        <Link href="/register">Sign Up</Link>
+      </Button>
+    </Stack>
+  );
+};
+
+const Navbar: FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { user } = useAuth();
+
+  const isLoggedIn = user !== undefined && user !== null;
+
+  return (
+    <>
+      <Box
+        bg={useColorModeValue('gray.100', 'primary')}
+        borderBottom={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        px={4}
+      >
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box>
+            <Link href="/">
+              <Text
+                fontFamily={'heading'}
+                fontWeight={'semibold'}
+                fontSize={'l'}
+                color={useColorModeValue('gray.800', 'white')}
+              >
+                Stream2Gather
+              </Text>
+            </Link>
+          </Box>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              {isLoggedIn ? <UserMenu /> : <LoginItems />}
+            </Stack>
+          </Flex>
+        </Flex>
       </Box>
-
-      <Spacer />
-
-      <HStack spacing="12px">
-        <ThemeToggler />
-        {isLoggedIn && (
-          <>
-            <Text fontSize="l" fontWeight={'semibold'}>
-              {user?.username}
-            </Text>
-
-            <PrimaryButton title="Logout" onClick={logout} />
-          </>
-        )}
-
-        {!isLoggedIn && (
-          <>
-            <Link href="/login">
-              <Button minWidth={'100px'} size="md" colorScheme="blue">
-                Login
-              </Button>
-            </Link>
-
-            <Link href="/register">
-              <Button minWidth={'100px'} size="md" colorScheme="blue">
-                Register
-              </Button>
-            </Link>
-          </>
-        )}
-      </HStack>
-    </Flex>
+    </>
   );
 };
 
--- frontend/src/components/ui/theme/ThemeToggler.tsx
+++ frontend/src/components/ui/theme/ThemeToggler.tsx
@@ -1,4 +1,4 @@
-import { useColorMode } from '@chakra-ui/react';
+import { Button, useColorMode } from '@chakra-ui/react';
 import React from 'react';
 import { animated, useSpring } from 'react-spring';
 
@@ -32,50 +32,51 @@ export const ThemeToggler: React.FC = () => {
   const linesProps = useSpring({ opacity });
 
   return (
-    <animated.svg
-      className="laptop:-mt-2 select-none"
-      xmlns="http://www.w3.org/2000/svg"
-      width="30"
-      height="30"
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      stroke={isDarkMode ? '#929ac9' : 'orange'}
-      onClick={toggleColorMode}
-      style={{
-        cursor: 'pointer',
-        ...svgContainerProps
-      }}
-    >
-      <mask id="myMask2">
-        <rect x="0" y="0" width="100%" height="100%" fill="white" />
-        <animated.circle
-          cx={maskedCircleProps.cx}
-          cy={maskedCircleProps.cy}
-          r="9"
-          fill="black"
-        />
-      </mask>
+    <Button onClick={toggleColorMode}>
+      <animated.svg
+        className="laptop:-mt-2 select-none"
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+        viewBox="0 0 24 24"
+        fill="none"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke={isDarkMode ? '#929ac9' : 'orange'}
+        style={{
+          cursor: 'pointer',
+          ...svgContainerProps
+        }}
+      >
+        <mask id="myMask2">
+          <rect x="0" y="0" width="100%" height="100%" fill="white" />
+          <animated.circle
+            cx={maskedCircleProps.cx}
+            cy={maskedCircleProps.cy}
+            r="9"
+            fill="black"
+          />
+        </mask>
 
-      <animated.circle
-        cx="12"
-        cy="12"
-        r={centerCircleProps.r}
-        fill={isDarkMode ? '#929ac9' : 'orange'}
-        mask="url(#myMask2)"
-      />
-      <animated.g stroke="orange" style={linesProps}>
-        <line x1="12" y1="1" x2="12" y2="3" />
-        <line x1="12" y1="21" x2="12" y2="23" />
-        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-        <line x1="1" y1="12" x2="3" y2="12" />
-        <line x1="21" y1="12" x2="23" y2="12" />
-        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-      </animated.g>
-    </animated.svg>
+        <animated.circle
+          cx="12"
+          cy="12"
+          r={centerCircleProps.r}
+          fill={isDarkMode ? '#929ac9' : 'orange'}
+          mask="url(#myMask2)"
+        />
+        <animated.g stroke="orange" style={linesProps}>
+          <line x1="12" y1="1" x2="12" y2="3" />
+          <line x1="12" y1="21" x2="12" y2="23" />
+          <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+          <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+          <line x1="1" y1="12" x2="3" y2="12" />
+          <line x1="21" y1="12" x2="23" y2="12" />
+          <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+          <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+        </animated.g>
+      </animated.svg>
+    </Button>
   );
 };

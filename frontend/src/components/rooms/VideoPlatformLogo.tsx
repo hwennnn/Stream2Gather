b7c41a352e5559ce1876db0d47fb71452babec04
hwen <hwendev@gmail.com>
2023-01-15 11:17:04 +0800

    display the VideoPlatformLogo inline with the text

--- frontend/src/components/rooms/VideoPlatformLogo.tsx
+++ frontend/src/components/rooms/VideoPlatformLogo.tsx
@@ -4,7 +4,16 @@ import { BsYoutube } from 'react-icons/bs';
 
 const VideoPlatformLogo: FC<{ platform: string }> = ({ platform }) => {
   if (platform === 'YOUTUBE') {
-    return <Icon mr={2} as={BsYoutube} color="red" />;
+    return (
+      <Icon
+        height={4}
+        width={4}
+        verticalAlign="top"
+        mr={2}
+        as={BsYoutube}
+        color="red"
+      />
+    );
   }
 
   return <Box></Box>;

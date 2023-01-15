86f80073f85820c669b7e2bb12d9fbc29608b814
hwen <hwendev@gmail.com>
2023-01-15 10:14:42 +0800

    revamp the design of QueryVideoCard

--- frontend/src/components/rooms/QueryVideoCard.tsx
+++ frontend/src/components/rooms/QueryVideoCard.tsx
@@ -4,21 +4,18 @@ import {
   Button,
   Card,
   CardBody,
-  CardFooter,
-  Flex,
   Icon,
   Image,
   Text,
   useColorModeValue
 } from '@chakra-ui/react';
 import { FC } from 'react';
-import { BsFillPlayFill } from 'react-icons/bs';
 import { MdPlaylistAdd } from 'react-icons/md';
 
 interface QueryVideoCardProps {
   video: VideoInfo;
-  onClickPlay?: () => void;
-  onClickAdd?: () => void;
+  onClickPlay: () => void;
+  onClickAdd: () => void;
 }
 
 export const QueryVideoCard: FC<QueryVideoCardProps> = ({
@@ -27,38 +24,72 @@ export const QueryVideoCard: FC<QueryVideoCardProps> = ({
   onClickAdd
 }) => {
   return (
-    <>
-      <Card w="full">
-        <CardBody p="0">
-          <Image
-            w="full"
-            src={video.thumbnailUrl}
-            alt={video.title}
-            borderRadius="sm"
-          />
+    <Card
+      w="full"
+      cursor="pointer"
+      onClick={onClickPlay}
+      _hover={{
+        '#cardOverlay': {
+          opacity: 1
+        }
+      }}
+    >
+      <Box
+        zIndex={1}
+        id="cardOverlay"
+        opacity={0}
+        pos="absolute"
+        top={0}
+        right={0}
+        onClick={(event) => {
+          event.stopPropagation();
+          onClickAdd();
+        }}
+      >
+        <Button
+          mt={1}
+          mr={1}
+          bg="black"
+          px={2}
+          borderRadius="md"
+          flex="1"
+          _hover={{
+            opacity: 1
+          }}
+        >
+          <Icon w={6} h={6} as={MdPlaylistAdd} />
+        </Button>
+      </Box>
+      <CardBody p="0">
+        <Image
+          w="full"
+          src={video.thumbnailUrl}
+          alt={video.title}
+          borderRadius="sm"
+        />
 
-          <Box p="2">
-            <Text
-              color={useColorModeValue('gray.700', 'gray.50')}
-              lineHeight={'shorter'}
-              fontSize={{ base: 'xs', lg: 'sm' }}
-              noOfLines={3}
-            >
-              {video.title}
-            </Text>
+        <Box p="2">
+          <Text
+            color={useColorModeValue('gray.700', 'gray.50')}
+            lineHeight={'shorter'}
+            fontSize={{ base: 'sm', lg: 'sm' }}
+            noOfLines={3}
+          >
+            {video.title}
+          </Text>
 
-            <Text
-              mt="2"
-              color={useColorModeValue('gray.600', 'gray.200')}
-              fontSize={{ base: 'sm', lg: '11px' }}
-              noOfLines={1}
-            >
-              {video.author}
-            </Text>
-          </Box>
-        </CardBody>
+          <Text
+            mt="2"
+            color={useColorModeValue('gray.600', 'gray.200')}
+            fontSize={{ base: 'xs', lg: '11px' }}
+            noOfLines={1}
+          >
+            {video.author}
+          </Text>
+        </Box>
+      </CardBody>
 
-        <CardFooter px="0" pb="0" pt="2">
+      {/* <CardFooter px="0" pb="0" pt="2">
           <Flex w="full" justify="space-between" flexWrap="wrap">
             <Button onClick={onClickPlay} borderRadius="md" flex="1" mr="1">
               <Icon w={6} h={6} as={BsFillPlayFill} />
@@ -67,8 +98,7 @@ export const QueryVideoCard: FC<QueryVideoCardProps> = ({
               <Icon w={6} h={6} as={MdPlaylistAdd} />
             </Button>
           </Flex>
-        </CardFooter>
-      </Card>
-    </>
+        </CardFooter> */}
+    </Card>
   );
 };
